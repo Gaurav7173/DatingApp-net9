4b154fec2273f71aa21446652f1ec1ce877dd264
Gaurav7173 <gaurav.kumar7173.com>
2025-05-24 12:01:36 +0530

    Adding the likes feature section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,12 +8,12 @@ public class AppUser
     public int Id { get; set; }
     public required string UserName { get; set; }
 
-    public byte[] PasswordHash { get; set; }=[];
-    public  byte[] PasswordSalt { get; set; }=[];
+    public byte[] PasswordHash { get; set; } = [];
+    public byte[] PasswordSalt { get; set; } = [];
     public DateOnly DateOfBirth { get; set; }
     public required string KnownAs { get; set; }
     public DateTime Created { get; set; } = DateTime.UtcNow;
-    public DateTime LastActive { get; set; }=DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
     public required string Gender { get; set; }
     public string? Introduction { get; set; }
     public string? Interests { get; set; }
@@ -21,9 +21,8 @@ public class AppUser
     public required string City { get; set; }
     public required string Country { get; set; }
     public List<Photo> Photos { get; set; } = [];
-    // public int GetAge()
-    // {
-    //     return DateOfBirth.CalculateAge();
-    // }
+    public List<UserLike> LikedByUsers { get; set; } = [];
+    public List<UserLike> LikedUsers { get; set; } = [];
+    
 
 }
